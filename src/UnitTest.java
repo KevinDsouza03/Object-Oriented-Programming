0b15213382b4118b61ee2585e463cc6295dce4aa
Kevin <kevindsouza476@gmail.com>
2023-02-22 16:13:47 -0500

    With Output

--- src/UnitTest.java
+++ src/UnitTest.java
@@ -13,12 +13,12 @@ public class UnitTest {
 		answers = new ArrayList<String>();
 		try {
 			Scanner input = new Scanner(new File("C:\\Users\\dsouz\\Documents\\csc330\\Lab2-Autograder\\src\\answers")).useDelimiter(" ");
-			//reading in from full path.
+			// reading in from full path.
 			String ans = new String(); // using a string to add the entire line, without spaces
 			while (input.hasNext()) {
 				answers.add(input.next());
 			} // now all answers are in the ArrayList<string>
-			questionStats = new double[answers.size()]; //this is used later to display the percentage of students that got a question right
+			questionStats = new double[answers.size()]; // this is used later to display the percentage of students that got a question right
 			input.close(); 
 		} catch (Exception e) { // cant allow the program to continue if we cant find the file
 			System.out.println("File Error. Ending UnitTest");
@@ -30,7 +30,7 @@ public class UnitTest {
 	//Methods
 	
 	public double calculateGrade(ArrayList<String> student_answers) {
-		//this function will take in a single students answer sheet, and then check each individual character for equals
+		// this function will take in a single students answer sheet, and then check each individual character for equals
 		double correct  = 0;
 		for (int i = 0; i < student_answers.size(); i++) {
 			if (answers.get(i).equals(student_answers.get(i))) {
@@ -38,18 +38,18 @@ public class UnitTest {
 				questionStats[i]++;// this is kept for our questionsRight function
 			}
 		}
-		//after calculating correct:
+		// after calculating correct:
 		return ((correct / answers.size()) * 100);
-		//simple math for calculating the percentage grade
+		// simple math for calculating the percentage grade
 	}
 	public void questionsRight(ArrayList<Student> Class) {
 		System.out.println("Below is the breakdown of what percent of students got a question right: ");
 		for (int i = 0; i < answers.size(); i++) {
 			System.out.printf("Question " + (i+1) + ": %.2f%% \n", questionStats[i] / Class.size() * 100);
-			//this function references an array of doubles that has index 0-9, or the number of questions from the test.
-			//the index's hold the number of correct answers from the entire class.
-			//finally, we pass the "Class" of Student to get a total number of all students, and get the percentage
-			//of students getting a question correct.
+			// this function references an array of doubles that has index 0-9, or the number of questions from the test.
+			// the index's hold the number of correct answers from the entire class.
+			// finally, we pass the "Class" of Student to get a total number of all students, and get the percentage
+			// of students getting a question correct.
 		}
 	}
 	
--- src/UnitTestRunner.java
+++ src/UnitTestRunner.java
@@ -5,42 +5,39 @@ import java.util.HashMap;
 public class UnitTestRunner {
 	public static void main(String args[])  {
 		UnitTest test = new UnitTest();
-		//automatically reads in from answers.txt
+		// automatically reads in from answers.txt
 		
 		ArrayList<Student> Class_Info = new ArrayList<Student>();
-		//preparing ArrayList of Student for file input
+		// preparing ArrayList of Student for file input
 		
-		//prepare hashmap
+		// prepare hashmap
 		
 		HashMap<String,ArrayList<String>> Map = new HashMap<String,ArrayList<String>>();
-		int offset = 0;
-		//reading in from allExams
+		// reading in from allExams
 		try {
 			Scanner student_info = new Scanner(new File("C:\\Users\\dsouz\\Documents\\csc330\\Lab2-Autograder\\src\\allExams")).useDelimiter(" ");
 			while (student_info.hasNext()) {
-				//read entire line, and then we filter it now
-				String[] full_info = student_info.nextLine().split("\\s");
+				// read entire line, and then we filter it now
+				String[] full_info = student_info.nextLine().split("\\s"); // splits the line into space seperated, so we can isolate names and answers.
 				Class_Info.add(new Student(full_info[1],full_info[0]));
-				//so here, we enter the first and last name into a string array, split by spaces.
-				//then we declare a new student object to add into our student ArrayList
-				//now we will grade, and input an average.
+				// so here, we enter the first and last name into a string array, split by spaces.
+				// then we declare a new student object to add into our student ArrayList
+				// now we will grade, and input an average.
 				// from 2 to end of LINE, we can read so any number of answers is allowed
 				int i = 2;
-				ArrayList<String> student_answers = new ArrayList<String>();
+				ArrayList<String> student_answers = new ArrayList<String>(); // temporary to insert into hashmap.
 				String answers_temp = new String();
 				while(i < full_info.length) {
-					answers_temp = full_info[i];
+					answers_temp = full_info[i]; // answers is just an appended string of student answers.
 					i++;
-					student_answers.add(answers_temp);
+					student_answers.add(answers_temp); 
 				} // what this does, is from the read in full_info str array, we already extracted the first and last name on index's 1 and 0.
 				// then, we know 2 is the next index, and the length returns us the index of the LAST value, which by our dataset, is the last
 				// answers for the respective student. now all we do is insert this into our string arraylist
 
 				Map.put((full_info[0] + full_info[1]).toLowerCase(),student_answers);
-				//now we enter the answers into the hashmap.
+				// now we enter the answers into the hashmap.
 				
-				//time to calculategrade
-				//so for calling calcgrade: setAverage with calling calcuateGrade on each object, using the Hashmap Key to reference the answers
 				}
 			student_info.close();
 		} catch (Exception e){
@@ -48,11 +45,16 @@ public class UnitTestRunner {
 			e.printStackTrace();
 			return;
 		}
-		
+		// time to calculategrade
+		// so for calling calcgrade: setAverage with calling calcuateGrade on each object, using the Hashmap Key to reference the answers
 		for (int i = 0; i < Class_Info.size(); i++) {
-			//System.out.println(test.calculateGrade(Map.get((Class_Info.get(i).getLastName() + Class_Info.get(i).getFirstName()).toLowerCase())));
 			Class_Info.get(i).setAverage(test.calculateGrade(Map.get((Class_Info.get(i).getLastName() + Class_Info.get(i).getFirstName()).toLowerCase())));
 			System.out.println("Student Name: " + Class_Info.get(i).getFirstName() +  " " + Class_Info.get(i).getLastName() + " | Average: " + Class_Info.get(i).getAverage());
+			// This entire call is essentially:
+			// We call the respective student object " Class_Info.get(i)" , followed by calling its setAverage function
+			// Then, the double is calculated by calling the Hashmap's value for Class_Info(i)'s student
+			// and finally, converting it to a lowercase. Essentially, the hashing algorithm
+			// Afterwards, we print out the the grades formatted.
 		}
 		test.questionsRight(Class_Info);
 	}
