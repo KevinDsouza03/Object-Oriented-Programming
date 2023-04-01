72c5f64efee1a2868a59089ee2f7a65551b6bb77
Kevin <kevindsouza476@gmail.com>
2023-04-01 11:25:48 -0400

    little added

--- Lab4 - Pokemon/src/Player.java
+++ Lab4 - Pokemon/src/Player.java
@@ -1,7 +1,7 @@
 
 public abstract class Player{ 
 
-	private PokemonElectric partyMember; //pokemon will be a type such as ElectricPokemon
+	private Pokemon partyMember; //pokemon will be a type such as ElectricPokemon
 	private Item item;
 	public Player(Pokemon p, Item i) {
 		// TODO Auto-generated constructor stub
--- Lab4 - Pokemon/src/PokemonRunner.java
+++ Lab4 - Pokemon/src/PokemonRunner.java
@@ -3,7 +3,6 @@ public class PokemonRunner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("i ran");
 	}
 
 }
