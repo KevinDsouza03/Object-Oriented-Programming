5e6a46f008d5fa5e379c590ce3495f84519faca8
Kevin <kevindsouza476@gmail.com>
2023-04-03 11:10:40 -0400

    types finsihed

--- Lab4 - Pokemon/src/PokemonRunner.java
+++ Lab4 - Pokemon/src/PokemonRunner.java
@@ -2,7 +2,10 @@
 public class PokemonRunner {
 
 	public static void main(String[] args) {
+		Type dogs = Type.BUG;
+		System.out.println(dogs.getType());
 		// TODO Auto-generated method stub
 	}
+
 
 }
--- Lab4 - Pokemon/src/Type.java
+++ Lab4 - Pokemon/src/Type.java
@@ -5,12 +5,12 @@
  * by default, all is private
  */
 enum Type {
-		Bug("Bug"), Dragon("Dragon"), Electric("Electric"), 
-		Fighting("Fighting"), Fire("Fire"),
-		Flying("Flying"), Ghost("Ghost"), 
-		Grass("Grass"), Ground("Ground"), Ice("Ice"), 
-		Normal("Normal"), Poison("Posion"), 
-		Psychic("Psychic"), Rock("Rock"), Water("Water");	
+		BUG("Bug"), DRAGON("Dragon"), ELECTRIC("Electric"), 
+		FIGHTING("Fighting"), FIRE("Fire"),
+		FLYING("Flying"), GHOST("Ghost"), 
+		GRASS("Grass"), GROUND("Ground"), ICE("Ice"), 
+		NORMAL("Normal"), POISON("Posion"), 
+		PSYCHIC("Psychic"), ROCK("Rock"), WATER("Water");	
 	
 		String element;
 		
@@ -19,4 +19,53 @@ enum Type {
 		}
 		//public accessor for type
 		public String getType() {return element;}
+		//isSupereffective()
+		public double isSuperEffective(Type defender) {
+			switch (this) { //asking is my move SE or NE
+			case BUG: if(defender == GRASS || defender == POISON || defender == PSYCHIC) return 1.5; 
+				if (defender == FIRE || defender == FLYING || defender == ROCK) return .5;
+				return 1;
+			case DRAGON: return 1;
+			case ELECTRIC: if (defender == FLYING || defender == WATER) return 1.5;
+				if (defender == ELECTRIC || defender == GRASS || defender == PSYCHIC) return .5;
+				if (defender == GROUND) {return 0;}
+				return 1;
+			case FIGHTING: if (defender == ICE || defender == NORMAL || defender == ROCK) return 1.5;
+				if (defender == FLYING || defender == PSYCHIC) return .5;
+				if (defender == GHOST) return 0;
+				return 1;
+			case FIRE: if (defender == BUG || defender == GRASS || defender == ICE) return 1.5;
+				if (defender == ROCK || defender == WATER) return .5;
+				return 1;
+			case FLYING: if (defender == BUG || defender == FIGHTING || defender == GRASS) return 1.5;
+				if (defender == ELECTRIC || defender == ROCK) return .5;
+				return 1;
+			case GHOST: if (defender == NORMAL || defender == PSYCHIC) return 0;
+				return 1;
+			case GRASS: if (defender == GROUND || defender == ROCK || defender == WATER) return 1.5;
+				if (defender == BUG || defender == FIRE || defender == FLYING || defender == GRASS || defender == POISON) return .5;
+				return 1;
+			case GROUND: if (defender == ELECTRIC || defender == FIRE || defender == POISON || defender == ROCK) return 1.5;
+				if(defender == GRASS) return .5;
+				if (defender == FLYING) return 0;
+				return 1;
+			case ICE: if (defender == DRAGON || defender == FLYING || defender == GRASS || defender == GROUND) return 1.5;
+				if (defender == ICE || defender == WATER) return .5;
+				return 1;
+			case NORMAL: if (defender== GHOST) return 0;
+				return 1;
+			case POISON: if(defender == BUG || defender == GRASS) return 1.5;
+				if (defender == GROUND || defender == POISON || defender == ROCK) return .5;
+				return 1;
+			case PSYCHIC: if (defender == FIGHTING || defender == POISON) return 1.5;
+				if (defender == PSYCHIC) return .5;
+				return 1;
+			case ROCK: if (defender == BUG || defender == FIRE || defender == FLYING || defender == ICE) return 1.5;
+				if (defender == FIGHTING || defender == ROCK) return .5;
+				return 1;
+			case WATER: if (defender == FIRE || defender == GROUND || defender == ROCK) return 1.5;
+				if (defender == GRASS || defender == ICE) return .5;
+				return 1;
+				
+		}
 	}
